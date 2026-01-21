64a8e0511aef78748c0d8638980a91255d8d7ed0
wpsy50 <wpsy50@durham.ac.uk>
2026-01-21 14:01:41 +0000

    Added GET /api/albums endpoint

---
+++ back_end/server.js
@@ -0,0 +1,41 @@
+import express from 'express';
+const app = express();
+const PORT = 3000
+
+app.use(express.static('public'));
+app.use(express.json());
+
+let albums = 
+[
+  { album_id: 1, user_id: 1, title: 'Concerts', description: 'Live concert photos', created_at: '01-08-2025', last_updated: '24-11-2025' }
+];
+
+let photos = 
+[
+  { photo_id: 1, album_id: 1, user_id: 1, url: 'radiohead.jpg', caption: 'Radiohead in the O2 arena', uploaded_at: '24-11-2025' },
+]
+
+app.get('/api/test', (req, res) => 
+{
+  res.json({ message: 'Server is working!' });
+});
+
+app.listen(PORT, () => 
+{
+  console.log(`Server running at http://localhost:${PORT}`);
+});
+
+app.get('/api/albums', (req, res) =>
+{
+  const album_list = albums.map(a => 
+    ({
+      album_id: a.album_id,
+      user_id: a.user_id,
+      title: a.title,
+      description: a.description,
+      created_at: a.created_at,
+      last_updated: a.last_updated
+    })
+  );
+  res.json(album_list);
+});
